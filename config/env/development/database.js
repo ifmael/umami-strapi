186d9d9aca1b7fd72eb9a554b34b256e790a5849
Ismael Rodriguez <ismael.rm.1986@gmail.com>
2020-12-29 20:12:16 +0100

    Add dbname

--- config/env/development/database.js
+++ config/env/development/database.js
@@ -5,6 +5,7 @@ module.exports = ({ env }) => ({
       connector: "mongoose",
       settings: {
         uri: env("DATABASE_URI"),
+        database: env("DATABASE_NAME"),
       },
       options: {},
     },
