8e8557edc311bfa9e4a734877390d19e4df269a5
Ismael Rodriguez <ismael.rm.1986@gmail.com>
2020-12-30 17:24:57 +0100

    Try another server

--- config/env/development/database.js
+++ config/env/development/database.js
@@ -1,14 +1,35 @@
+// module.exports = ({ env }) => ({
+//   defaultConnection: "default",
+//   connections: {
+//     default: {
+//       connector: "mongoose",
+//       settings: {
+//         uri: env("DATABASE_URI"),
+//         // database: env("DATABASE_NAME"),
+//       },
+//       options: {
+//         ssl: true,
+//       },
+//     },
+//   },
+// });
+
 module.exports = ({ env }) => ({
   defaultConnection: "default",
   connections: {
     default: {
       connector: "mongoose",
       settings: {
-        uri: env("DATABASE_URI"),
-        // database: env("DATABASE_NAME"),
+        host: env("DATABASE_HOST", "127.0.0.1"),
+        srv: env.bool("DATABASE_SRV", false),
+        port: env.int("DATABASE_PORT", 27017),
+        database: env("DATABASE_NAME", "umami-production"),
+        username: env("DATABASE_USERNAME", null),
+        password: env("DATABASE_PASSWORD", null),
       },
       options: {
-        ssl: true,
+        authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
+        ssl: env.bool("DATABASE_SSL", false),
       },
     },
   },
