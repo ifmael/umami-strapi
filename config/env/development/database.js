e453f3857aed6d5c92912c66a5a955970030af09
Ismael Rodriguez <ismael.rm.1986@gmail.com>
2020-12-30 06:27:11 +0100

    remove database name variable

--- config/env/development/database.js
+++ config/env/development/database.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
       connector: "mongoose",
       settings: {
         uri: env("DATABASE_URI"),
-        database: env("DATABASE_NAME"),
+        // database: env("DATABASE_NAME"),
       },
       options: {},
     },
