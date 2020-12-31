d39cf736da36015015c7523a8141e8b23a0f8344
Ismael Rodriguez <ismael.rm.1986@gmail.com>
2020-12-31 10:24:19 +0100

    Add S3 conf bucket

--- config/database.js
+++ config/database.js
@@ -1,30 +1,19 @@
-// module.exports = ({ env }) => ({
-//   defaultConnection: "default",
-//   connections: {
-//     default: {
-//       connector: "mongoose",
-//       settings: {},
-//       options: {},
-//     },
-//   },
-// });
-
 module.exports = ({ env }) => ({
   defaultConnection: "default",
   connections: {
     default: {
       connector: "mongoose",
       settings: {
-        host: env("DATABASE_HOST", "127.0.0.1"),
-        srv: env.bool("DATABASE_SRV", false),
-        port: env.int("DATABASE_PORT", 27017),
-        database: env("DATABASE_NAME", "umami-project-local"),
-        username: env("DATABASE_USERNAME", null),
-        password: env("DATABASE_PASSWORD", null),
+        // host: env("DATABASE_HOST", "127.0.0.1"),
+        // srv: env.bool("DATABASE_SRV", false),
+        // port: env.int("DATABASE_PORT", 27017),
+        // database: env("DATABASE_NAME", "umami-burger-local"),
+        // username: env("DATABASE_USERNAME", null),
+        // password: env("DATABASE_PASSWORD", null),
       },
       options: {
-        authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
-        ssl: env.bool("DATABASE_SSL", false),
+        // authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
+        // ssl: env.bool("DATABASE_SSL", false),
       },
     },
   },
