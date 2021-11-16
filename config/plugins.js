20b1598dfd3a9298317ad63ea28a9dabaa339462
ismael Rodriguez <ismael.rm.1986@gmail.com>
2021-11-16 19:07:01 +0100

    build(aws): :memo: Change environment  variables names for aws to avoid conflict with vercel environment variable

--- config/plugins.js
+++ config/plugins.js
@@ -2,8 +2,8 @@ module.exports = ({ env }) => ({
   upload: {
     provider: "aws-s3",
     providerOptions: {
-      accessKeyId: env("AWS_ACCESS_KEY_ID"),
-      secretAccessKey: env("AWS_ACCESS_SECRET"),
+      accessKeyId: env("AWS_ACCESS_KEY_ID_UMAMI"),
+      secretAccessKey: env("AWS_ACCESS_SECRET_UMAMI"),
       region: "eu-west-1",
       params: {
         Bucket: "umami-images",
